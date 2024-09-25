e8b53b1b9feb0fdd74168cab3bdaa6197e46ed30
joseGalvan06 <163416742+joseGalvan06@users.noreply.github.com>
2024-09-25 08:43:46 -0300

    dsa

---
+++ my-app/src/app/Login/page.js
@@ -0,0 +1,9 @@
+import PaginaLoginRegistro from "./paginaLogin";
+
+export default function Home() {
+    return (
+        <div>
+            <PaginaLoginRegistro></PaginaLoginRegistro>
+        </div>
+    )
+}
---
+++ my-app/src/app/Login/paginaLogin.jsx
@@ -0,0 +1,12 @@
+"use client"
+import './styles.css'
+
+export default function PaginaLoginRegistro(){
+    return(
+        <div className='fondo1'>
+            <div>
+                <image src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/640px-International_Pok%C3%A9mon_logo.svg.png'></image>
+            </div>
+        </div>
+    )
+}
