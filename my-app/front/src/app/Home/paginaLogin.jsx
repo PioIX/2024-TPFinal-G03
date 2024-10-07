6f284c516ad7a80d3a0b8b18adc04e68391d764c
joseGalvan06 <163416742+joseGalvan06@users.noreply.github.com>
2024-10-07 08:43:08 -0300

    fdsfds

--- my-app/front/src/app/Home/paginaLogin.jsx
+++ my-app/front/src/app/Home/paginaLogin.jsx
@@ -1,23 +1,28 @@
 "use client"
 import './styles.css'
+import { useRouter } from 'next/navigation'
+
 import Image from 'next/image'
 
-
 export default function PaginaLoginRegistro(){
+    
+    const router = useRouter()
+
     return(
         <div className='fondo1'>
             <div>
                 <Image 
-                    style={{paddingLeft:"110px"}}
+                    style={{paddingLeft:"180px"}}
                     src="/imagenes/pokemonLogo.png"
-                    width={680}
+                    width={780}
                     height={205}
                     alt='hols'
                 /> 
                 <div>
                     <h4 className='subtitulo1'>Chowdawn</h4>
                     <div className='botonesLogin'>
-                        <button className='botonRegistroLogin'>Login</button>
+                        <button className='botonRegistroLogin' onClick={() => router.push('@/Login')}>Iniciar sesion</button>
+                        <button className='botonRegistroLogin' onClick={() => router.push('/Home/Registro')}>Registrarse</button>
                     </div>
                 </div>
             </div>
