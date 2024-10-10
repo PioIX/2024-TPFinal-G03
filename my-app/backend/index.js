5d81a9bb68e90f547e6efd340a044d1b2b5d0781
ç <163416742+joseGalvan06@users.noreply.github.com>
2024-10-10 10:08:41 -0300

    fdsfd

--- my-app/backend/index.js
+++ my-app/backend/index.js
@@ -1,18 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var cors = require('cors');
 
 
-var MySql = require('./modulos/mysql.js');
+var MySql = require('./sql/modulos/mysql.js');
 
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3001;
 
 
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-app.use(cors());
 
 // GET
 app.get('/', function(req, res){
