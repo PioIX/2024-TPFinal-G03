5c21c55cbb875773cfd969964e65f04f9c1d7af3
ç <163416742+joseGalvan06@users.noreply.github.com>
2024-10-23 13:05:12 -0300

    fdgf

--- my-app/front/src/app/Home/Registro/page.js
+++ my-app/front/src/app/Home/Registro/page.js
@@ -100,21 +100,21 @@ export default function PaginaRegistro(props){
 
     return(
         <div className='fondo2'>
-            <div>
+            <div className='display'>
                 <Image 
                     src="/imagenes/pokemonLogo.png"
                     width={280}
                     height={100}
                     alt='hols'
                 /> 
-                <div>
-                    <h4 className='subtitulo1'>Chowdawn</h4>
-                    <div className='botonesLogin'>
+                    <h4 className='subtitulo2'>Chowdawn</h4>
+                    <div className='divBotones'>
                         <input onChange={registrarNombre} className={"botonInput"} placeholder={"Ingrese el nombre"}></input>
+                        <p className='p'></p>
                         <input onChange={registrarContrasenia} className={"botonInput"} placeholder={"Ingrese la contraseña"}></input>
-                    </div>
                         <button className='botonRegistroLogin' onClick={registrarsePorBoton}>Registrarse</button>
-                </div>
+                    </div>
+                    
             </div>
         </div>
     )
