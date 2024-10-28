429f09a301ba81e0f664e8177c3edc398da0e290
joseGalvan06 <163416742+joseGalvan06@users.noreply.github.com>
2024-10-28 10:05:10 -0300

    gffggf

--- my-app/front/src/app/Home/Teambuilder/Pokemon.jsx
+++ my-app/front/src/app/Home/Teambuilder/Pokemon.jsx
@@ -1,26 +1,14 @@
 "use client"
 import '@/app/Home/styles.css'
-import { useRouter } from 'next/navigation'
 
-import Image from 'next/image'
 
-export default function Pokemon(){
+export default function Pokemon(props){
     return(
-        <div className='fondo3'>
-                <div className='display'>
-                <Image 
-                    src="/imagenes/pokemonLogo.png"
-                    width={280}
-                    height={100} 
-                    alt='hols'
-                /> 
-                <h4 className='subtitulo2'>Chowdawn</h4>
-                <div className='divPokemon'>
-                    <h2></h2>
-                    <h2>(Nombre pokemon)</h2>
-                    <h2>(Pokemon)</h2>
-                </div>
+
+        <div className='divPokemon'>
+            <h2>Nombre pokemon</h2>
+            <h2 nombrePokemon={props.nombrePokemon}></h2>
+            <h2>(Pokemon)</h2>
             </div>
-        </div>
     )
 }
--- my-app/front/src/app/Home/Teambuilder/page.js
+++ my-app/front/src/app/Home/Teambuilder/page.js
@@ -1,9 +1,24 @@
+"use client"
 import Pokemon from "./Pokemon";
+import { useRouter } from 'next/navigation'
+
+import Image from 'next/image'
 
 export default function Home() {
-    return (          
-        <div>
-            <Pokemon></Pokemon>
+    return ( 
+        <div className='fondo3'>
+            <div className='display'>
+                <Image 
+                    src="/imagenes/pokemonLogo.png"
+                    width={280}
+                    height={100} 
+                    alt='hols'
+                /> 
+                <h4 className='subtitulo2'>Chowdawn</h4>         
+                <div>
+                    <Pokemon></Pokemon>
+                </div>  
+            </div>
         </div>
     )
 }
