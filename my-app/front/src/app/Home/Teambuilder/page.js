15525dd6da16d8df3c8313d634f0c5901050ae79
joseGalvan06 <163416742+joseGalvan06@users.noreply.github.com>
2024-10-29 08:43:24 -0300

    dsfdsfdfs

--- my-app/front/src/app/Home/Teambuilder/page.js
+++ my-app/front/src/app/Home/Teambuilder/page.js
@@ -7,7 +7,7 @@ import Image from 'next/image'
 export default function Home() {
     return ( 
         <div className='fondo3'>
-            <div className='display'>
+            <div className='display2'>
                 <Image 
                     src="/imagenes/pokemonLogo.png"
                     width={280}
@@ -16,7 +16,24 @@ export default function Home() {
                 /> 
                 <h4 className='subtitulo2'>Chowdawn</h4>         
                 <div>
-                    <Pokemon></Pokemon>
+                    <Pokemon nombreEquipo={"rgbyiu"} pokemon1={"https://creativesharkco.com/cdn/shop/files/SpearowMiniSpritePixelArt.png?v=1719689997"}
+                    pokemon2={"https://creativesharkco.com/cdn/shop/files/SpearowMiniSpritePixelArt.png?v=1719689997"}
+                    pokemon3={"https://creativesharkco.com/cdn/shop/files/SpearowMiniSpritePixelArt.png?v=1719689997"}
+                    pokemon4={"https://creativesharkco.com/cdn/shop/files/SpearowMiniSpritePixelArt.png?v=1719689997"}
+                    pokemon5={"https://creativesharkco.com/cdn/shop/files/SpearowMiniSpritePixelArt.png?v=1719689997"}
+                    pokemon6={"https://creativesharkco.com/cdn/shop/files/SpearowMiniSpritePixelArt.png?v=1719689997"}></Pokemon>
+                    <Pokemon nombreEquipo={"rgbyiu"} pokemon1={"https://creativesharkco.com/cdn/shop/files/SpearowMiniSpritePixelArt.png?v=1719689997"}
+                    pokemon2={"https://creativesharkco.com/cdn/shop/files/SpearowMiniSpritePixelArt.png?v=1719689997"}
+                    pokemon3={"https://creativesharkco.com/cdn/shop/files/SpearowMiniSpritePixelArt.png?v=1719689997"}
+                    pokemon4={"https://creativesharkco.com/cdn/shop/files/SpearowMiniSpritePixelArt.png?v=1719689997"}
+                    pokemon5={"https://creativesharkco.com/cdn/shop/files/SpearowMiniSpritePixelArt.png?v=1719689997"}
+                    pokemon6={"https://creativesharkco.com/cdn/shop/files/SpearowMiniSpritePixelArt.png?v=1719689997"}></Pokemon>
+                    <Pokemon nombreEquipo={"rgbyiu"} pokemon1={"https://creativesharkco.com/cdn/shop/files/SpearowMiniSpritePixelArt.png?v=1719689997"}
+                    pokemon2={"https://creativesharkco.com/cdn/shop/files/SpearowMiniSpritePixelArt.png?v=1719689997"}
+                    pokemon3={"https://creativesharkco.com/cdn/shop/files/SpearowMiniSpritePixelArt.png?v=1719689997"}
+                    pokemon4={"https://creativesharkco.com/cdn/shop/files/SpearowMiniSpritePixelArt.png?v=1719689997"}
+                    pokemon5={"https://creativesharkco.com/cdn/shop/files/SpearowMiniSpritePixelArt.png?v=1719689997"}
+                    pokemon6={"https://creativesharkco.com/cdn/shop/files/SpearowMiniSpritePixelArt.png?v=1719689997"}></Pokemon>
                 </div>  
             </div>
         </div>
