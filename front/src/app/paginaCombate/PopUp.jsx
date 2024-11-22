640baa5e0ff2f348af7b852b507ff5d22b512c64
TitoCalderon <163416742+joseGalvan06@users.noreply.github.com>
2024-11-22 15:24:14 -0300

    uihk

--- front/src/app/paginaCombate/PopUp.jsx
+++ front/src/app/paginaCombate/PopUp.jsx
@@ -2,7 +2,7 @@
 
 export default function PopUp(props){
     return(
-        <dialog open={props.abrirse}>
+        <dialog open={props.abrirse} style={{width:"100%", height:"100%", backgroundColor:"blue"}}>
             <h3>{props.texto}</h3>
             <button onClick={props.funcion}>Volver al creador de equipos</button>
         </dialog>
